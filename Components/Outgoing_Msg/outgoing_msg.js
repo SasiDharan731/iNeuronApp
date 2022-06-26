1478c725185059f34dbe0efa6ef1ed6a87e70e43
Vishal-Techy <vishalchinns1650@gmail.com>
2022-06-26 07:42:03 +0530

    completed chatbot

---
+++ Components/Outgoing_Msg/outgoing_msg.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from '../../styles/outgoing_msg.module.css'
+
+function outgoing_msg() {
+  return (
+    <div className={styles.outgoing_card}>
+
+        <p className={styles.chat}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam?</p>
+
+    </div>
+  )
+}
+
+export default outgoing_msg
